7b29ab81fb1c523f79e6a9a4f9a8f6a664dbda88
kopvi46 <140708770+kopvi46@users.noreply.github.com>
2024-09-10 10:32:00 +0300

    completed animations

--- Assets/Scripts/PlayerAnimationController.cs
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.InputSystem.Utilities;
 
 public class PlayerAnimationController : MonoBehaviour
 {
@@ -10,13 +13,12 @@ public class PlayerAnimationController : MonoBehaviour
     private const string IS_WALKING_BACKWARD = "isWalkingBackward";
     private const string IS_WALKING_LEFT = "isWalkingLeft";
     private const string IS_WALKING_RIGHT = "isWalkingRight";
-    private const string IS_JUMPING = "isJumping";
+    private const string JUMP_TRIGGER = "jumpTrigger";
     private const string IS_RUNNING = "isRunning";
 
     private Animator _animator;
     private PlayerInputActions _playerInputActions;
-    private Vector2 _inputVector;
-    private bool _isJumping;
+    private Vector2 _inputMoveVector;
     private bool _isRunning;
 
     private void Start()
@@ -25,20 +27,15 @@ public class PlayerAnimationController : MonoBehaviour
         _playerInputActions = new PlayerInputActions();
         _playerInputActions.Player.Enable();
 
-        _playerInputActions.Player.Jump.started += OnJumpStarted;
-        _playerInputActions.Player.Jump.canceled += OnJumpCanceled;
+        PlayerMovement.Instance.OnJump += Player_OnJump;
+
         _playerInputActions.Player.Run.started += OnRunStarted;
         _playerInputActions.Player.Run.canceled += OnRunCanceled;
     }
 
-    private void OnJumpCanceled(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    private void Player_OnJump(object sender, System.EventArgs e)
     {
-        _isJumping = false;
-    }
-
-    private void OnJumpStarted(UnityEngine.InputSystem.InputAction.CallbackContext obj)
-    {
-        _isJumping = true;
+        _animator.SetTrigger(JUMP_TRIGGER);
     }
 
     private void OnRunCanceled(UnityEngine.InputSystem.InputAction.CallbackContext obj)
@@ -53,27 +50,16 @@ public class PlayerAnimationController : MonoBehaviour
 
     private void Update()
     {
-        _inputVector = _playerInputActions.Player.Movement.ReadValue<Vector2>();
+        _inputMoveVector = _playerInputActions.Player.Movement.ReadValue<Vector2>();
 
-        _animator.SetBool(IS_WALKING_FORWARD_LEFT, ((_inputVector.y > 0 && _inputVector.y < 1) && (_inputVector.x < 0 && _inputVector.x > -1)));
-        _animator.SetBool(IS_WALKING_FORWARD_RIGHT, ((_inputVector.y > 0 && _inputVector.y < 1) && (_inputVector.x > 0 && _inputVector.x < 1)));
+        _animator.SetBool(IS_WALKING_FORWARD_LEFT, ((_inputMoveVector.y > 0 && _inputMoveVector.y < 1) && (_inputMoveVector.x < 0 && _inputMoveVector.x > -1)));
+        _animator.SetBool(IS_WALKING_FORWARD_RIGHT, ((_inputMoveVector.y > 0 && _inputMoveVector.y < 1) && (_inputMoveVector.x > 0 && _inputMoveVector.x < 1)));
 
-        _animator.SetBool(IS_WALKING_FORWARD, _inputVector.y == 1);
-        _animator.SetBool(IS_WALKING_BACKWARD, _inputVector.y < 0);
-        _animator.SetBool(IS_WALKING_LEFT, _inputVector.x == -1);
-        _animator.SetBool(IS_WALKING_RIGHT, _inputVector.x == 1);
+        _animator.SetBool(IS_WALKING_FORWARD, _inputMoveVector.y == 1);
+        _animator.SetBool(IS_WALKING_BACKWARD, _inputMoveVector.y < 0);
+        _animator.SetBool(IS_WALKING_LEFT, _inputMoveVector.x == -1);
+        _animator.SetBool(IS_WALKING_RIGHT, _inputMoveVector.x == 1);
 
-        _animator.SetBool(IS_JUMPING, _isJumping);
         _animator.SetBool(IS_RUNNING, _isRunning);
-    }
-
-    private void ResetAllAnimations()
-    {
-        _animator.SetBool(IS_WALKING_FORWARD_LEFT, false);
-        _animator.SetBool(IS_WALKING_FORWARD_RIGHT, false);
-        _animator.SetBool(IS_WALKING_FORWARD, false);
-        _animator.SetBool(IS_WALKING_BACKWARD, false);
-        _animator.SetBool(IS_WALKING_LEFT, false);
-        _animator.SetBool(IS_WALKING_RIGHT, false);
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,24 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public static PlayerMovement Instance { get; private set; }
+
+    public event EventHandler OnJump;
+
+    private const string IDLE_ANIMATION = "Breathing Idle";
+
     [SerializeField] private float _jumpForce;
     [SerializeField] private float _speed;
+    [SerializeField] private float _runSpeedMultiplier;
     [SerializeField] private float _mouseSensitivity;
+    [SerializeField] private LayerMask _groundLayer;
 
+    private Animator _animator;
     private Rigidbody _rigidbody;
     private PlayerInputActions _playerInputActions;
     private float _currentRotation;
 
+    private void Awake()
+    {
+        Instance = this;
+    }
+
     private void Start()
     {
+        _animator = GetComponent<Animator>();
         _rigidbody = GetComponent<Rigidbody>();
         _playerInputActions = new PlayerInputActions();
 
         _playerInputActions.Player.Enable();
         _playerInputActions.Player.Jump.performed += Jump;
+        _playerInputActions.Player.Run.started += OnRunStarted;
+        _playerInputActions.Player.Run.canceled += OnRunCanceled;
+    }
+
+    private void OnRunCanceled(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        _speed = _speed / _runSpeedMultiplier;
+    }
+
+    private void OnRunStarted(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        _speed = _speed * _runSpeedMultiplier;
     }
 
     private void FixedUpdate()
@@ -46,7 +74,11 @@ public class PlayerMovement : MonoBehaviour
 
         float moveDistance = _speed * Time.deltaTime;
 
-        transform.position += directionVector * moveDistance;
+        if (!_animator.GetCurrentAnimatorStateInfo(0).IsName(IDLE_ANIMATION))
+        {
+            transform.position += directionVector * moveDistance;
+
+        }
     }
 
     private void HandleRotation()
@@ -62,6 +94,16 @@ public class PlayerMovement : MonoBehaviour
 
     private void Jump(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
-        _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        bool isGrounded;
+        float groundDistance = .1f;
+
+        isGrounded = Physics.CheckSphere(transform.position, groundDistance, _groundLayer);
+
+        if (isGrounded && _animator.GetCurrentAnimatorStateInfo(0).IsName(IDLE_ANIMATION))
+        {
+            _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+
+            OnJump?.Invoke(this, EventArgs.Empty);
+        }
     }
 }
